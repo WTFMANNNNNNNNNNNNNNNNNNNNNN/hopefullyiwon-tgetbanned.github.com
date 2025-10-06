f8f15af1b5b9cf475b78e92af872f65129d31688
WTFMANNNNNNNNNNNNNNNNNNNNNN <mnguy187@eq.edu.au>
2025-10-06 23:04:15 +1000

    Update config.js

--- server/config.js
+++ server/config.js
@@ -6,7 +6,7 @@ module.exports = {
     host: "hopefullyiwon-tgetbanned-github-com.onrender.com",
 
     // Which port to run the web server on.
-    port: 26301,
+    port: 3000,
 
     // How often to update the list of the entities that players can see.
     // Has effects of when entities are activated.
