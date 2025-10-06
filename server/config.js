c1cf8bc718da9a38892992715cd7ba9e5972a046
WTFMANNNNNNNNNNNNNNNNNNNNNN <mnguy187@eq.edu.au>
2025-10-06 22:56:51 +1000

    Update config.js

--- server/config.js
+++ server/config.js
@@ -3,7 +3,7 @@ module.exports = {
 
     // Game server domain.
     // If the host is 'localhost:NUMBER', the NUMBER must be the port setting.
-    host: "https://hopefullyiwon-tgetbanned-github-com.onrender.com:26301",
+    host: "hopefullyiwon-tgetbanned-github-com.onrender.com",
 
     // Which port to run the web server on.
     port: 26301,
